0a93be8ae0899383c52fdc9981064c6016c4748c
Helen <helen.pernu@skola.botkyrka.se>
2020-11-09 14:17:48 +0100

    First commit, solved 5

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Read instructions. And remove this line.
+const secretNumber = randomInt()
+
+function randomInt() {
+    return Math.floor(Math.random() * 100)
+}
+
+function getUserGuess() {
+    const stringValue = document.getElementById("user-input").value
+    return parseInt(stringValue, 10)
+}
+
+document.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        const guess = getUserGuess()
+        console.log(guess)
+
+        if (guess < secretNumber){
+            setMessage("Too low!")
+        }
+
+        else if (guess > secretNumber){
+            setMessage("Too high!")
+        }
+
+        else{
+            setMessage("You win!")
+        }
+        clearInput()
+    }
+})
+
+function setMessage(msg) {
+    document.getElementById("message").innerText = msg
+}
+
+function clearInput() {
+    document.getElementById("user-input").value = ""
+}
+
